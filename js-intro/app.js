d50dd3a4b02eaa23901753bacd3dfce3cb14b528
Christian Lisangola <jochri3@gmail.com>
2021-09-30 15:43:04 +0300

    declaration et typage

--- js-intro/app.js
+++ js-intro/app.js
@@ -3,3 +3,15 @@
 /*
 mutline comment
 */
+
+//Déclaration de variables
+let firstName = 'Jean'
+console.log(firstName)
+firstName = 'Jonathan'
+console.log(firstName)
+firstName = 'Lydie'
+console.log(firstName)
+
+//Typage dynamique
+let age = 67
+age = '89'
