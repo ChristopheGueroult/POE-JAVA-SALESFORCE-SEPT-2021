0079800f7bd7f48567aae15e9a12908ecdf1fe5d
Christian Lisangola <jochri3@gmail.com>
2021-09-30 16:20:43 +0300

    Type de données

--- js-intro/app.js
+++ js-intro/app.js
@@ -19,3 +19,23 @@ age = '89'
 //Constantes : Ces valeurs ne peuvent pas être modifiées
 const accelerationPesanteur = 9.8
 const pays = 'France'
+
+//Type de données
+//String
+const lastName = 'Jonathan'
+const firstName = 'Jean'
+const fullName = lastName + ' ' + firstName
+const fullName2 = `${lastName} ${firstName}`
+
+//Number
+const age = 67
+const ageDeMonPere = 12
+const ageDeMaMere = 9
+const sommeDesAges = ageDeMonPere + ageDeMaMere
+
+let nombre = 45
+let nombre2 = 26
+let somme = nombre + nombre2
+
+//Boolean
+const isMarried = true
