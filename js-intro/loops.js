9be6e851e45552bd180d16bc9bd0ff96400853d7
Christian Lisangola <jochri3@gmail.com>
2021-10-01 15:11:49 +0300

    boucle while(){...}

--- js-intro/loops.js
+++ js-intro/loops.js
@@ -25,3 +25,18 @@ const noms = 'Christian Lisangola'
 for (let i = 0; i < noms.length; i++) {
     console.log(noms[i])
 }
+
+//while{...}
+
+let votreMotDepasse = 'abcdefghijklm'
+
+while (votreMotDepasse.length < 8) {
+    votreMotDepasse = prompt('Veuillez saisir votre mot de passe : ')
+    if (votreMotDepasse.length < 8) {
+        alert('Le mot de passe doit contenir au moins 8 caractères')
+    } else {
+        alert('Le mot de passe respecte les requirements')
+    }
+}
+
+//do{...}while()
