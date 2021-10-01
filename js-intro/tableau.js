2b447a24f94ea6e42b9ea20744dfb03a9c1e565a
Christian Lisangola <jochri3@gmail.com>
2021-10-01 16:49:57 +0300

    creattion des tableau

---
+++ js-intro/tableau.js
@@ -0,0 +1,9 @@
+const fruits = new Array()
+
+//Tableau vide
+const eleves = []
+
+//Creation et initialisation
+const cours = ['Chimie', 'Physique', 'Grammaire']
+
+const monTableau = ['jean', false, 45]
