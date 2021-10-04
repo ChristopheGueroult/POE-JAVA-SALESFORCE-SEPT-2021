bfe3bb9a6efefee2cad7fe370411b4f5d7869e70
Christian Lisangola <jochri3@gmail.com>
2021-10-04 15:42:38 +0300

    Objets en JS

---
+++ js-intro/objets.js
@@ -0,0 +1,15 @@
+const TVA = 0.2
+
+const article = {
+    designation: 'T-shirt',
+    reference: 'xyz00pp2',
+    prixHT: 15,
+    calculerPrixTTC() {
+        return this.prixHT + this.prixHT * TVA
+    },
+}
+
+console.log(`Designation : ${article.designation}`)
+console.log(`Ref : ${article.reference}`)
+console.log(`Prix HT : ${article.prixHT}€`)
+console.log(`Prix TTC : ${article.calculerPrixTTC()}€`)
