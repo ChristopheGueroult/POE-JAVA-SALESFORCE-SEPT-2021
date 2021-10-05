35ae3de33aacd896e59888e03e8809ec84bc7e82
Christian Lisangola <jochri3@gmail.com>
2021-10-05 11:32:31 +0300

    Add comments

--- dom/app.js
+++ dom/app.js
@@ -19,14 +19,13 @@ for (let i = 0; i < para.length; i++) {
 }
 
 //Mettre en rouge tous les élements impairs de la liste des technos
-
+//On fait une boucle, parce que les propriétés n'existe par sur la collection, mais sur l'objet
 for (let i = 0; i < liste.length; i = i + 2) {
     liste[i].style.color = 'red'
     // liste[i].style.
 }
 
-//Accéder à une occurence
-
+//Accéder à une occurence.Contrairement à querySeletorAll() ici, on accède directement à l'objet et non la collection
 const p = document.querySelector('p')
 p.innerText = 'Je suis dans une joie immence'
 
@@ -36,6 +35,6 @@ const technos = document.querySelector('#technos')
 technos.innerHTML += '<li>Papapapa</li>'
 // console.log(technos.innerHTML);
 
-// Ajouter Kubernetes
+// Ajouter une balise avant les autres dans la liste
 const devops = document.querySelector('#devops')
 devops.innerHTML = '<li>Kubernetes</li>' + devops.innerHTML
